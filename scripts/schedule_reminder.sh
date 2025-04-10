db20164fed977ae75816c73bd8220371c3fbefb7
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-04-10 08:45:47 +0200

    Update schedule

--- scripts/schedule_reminder.sh
+++ scripts/schedule_reminder.sh
@@ -2,4 +2,5 @@
 echo 'espeak -s 120 -p 10 "start to work"' | at 9:00
 echo 'espeak -s 120 -p 10 "time to have a break"' | at 9:50
 echo 'espeak -s 120 -p 10 "back to work"' | at 10:00
+echo 'espeak -s 120 -p 10 "evaluatuion"' | at 10:20
 echo 'espeak -s 120 -p 10 "lesson is over"' | at 10:30
